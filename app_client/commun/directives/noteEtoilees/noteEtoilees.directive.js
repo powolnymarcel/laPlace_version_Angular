40995a0d1abfacd55ea2bfe31d7705820b479209
powolnymarcel <powolnymarcel@gmail.com>
2016-01-22 17:43:40 +0100

    mise en place directive note etoilees

---
+++ app_client/commun/directives/noteEtoilees/noteEtoilees.directive.js
@@ -0,0 +1,12 @@
+angular
+	.module('laPlaceApp')
+	.directive('noteEtoilees', noteEtoilees);
+function noteEtoilees () {
+	return {
+		restrict: 'EA',
+		scope: {
+			laNote : '=note'
+		},
+		templateUrl: '/commun/directives/noteEtoilees/note-etoilees.template.html'
+	};
+}
